ba4534b524a9807a9709c3e57078c6f4645d38cf
coskunergan <coskunergan@gmail.com>
2022-10-27 22:42:49 +0300

    V1.0.1
    -Code test.

--- main.c
+++ main.c
@@ -10,12 +10,12 @@
 #include <pic16regs.h>
 #include <stdint.h>
 
-__code uint16_t __at (_CONFIG) __configword = _INTRC_OSC_NOCLKOUT & _WDTE_OFF;
+__code uint16_t __at (_CONFIG)   __configword = 0xF304;
 
-#define LED_PORT GPIObits.GP0
-#define LED_TRIS TRISIObits.TRISIO0
+#define LED_PORT GPIObits.GP2
+#define LED_TRIS TRISIObits.TRISIO2
 
-void delay(volatile uint16_t iterations)
+void delay(uint16_t iterations)
 {
     for(uint16_t i = 0; i < iterations; i++)
     {
@@ -23,16 +23,21 @@ void delay(volatile uint16_t iterations)
     }
 }
 
-void main(void)
+int main(void)
 {
-    LED_TRIS = 0;
-   	LED_PORT = 0;	
-
+    ANSELbits.ANS=0;          
+    ANSELbits.ADCS=0;   
+    CMCON0bits.CM=0x7;          
+    // LED_TRIS = 0;
+   	// LED_PORT = 0;
+    TRISIObits.TRISIO=0;	
     while(1)
     {
-        LED_PORT = 1;
-        delay(30000);
-        LED_PORT = 0;
-        delay(30000);
+        // LED_PORT = 1;
+        GPIObits.GP=255;
+        delay(300);
+        // LED_PORT = 0;
+        GPIObits.GP=0;
+        delay(300);
     }
 }
